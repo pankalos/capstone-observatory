c5ecdd27ccb81ac59ff35d0a59685b9371e1e3c7
pankalos <pan_kal_os@hotmail.com>
2017-09-05 19:24:19 +0300

    Update Extraction.scala

--- src/main/scala/observatory/Extraction.scala
+++ src/main/scala/observatory/Extraction.scala
@@ -8,9 +8,9 @@ package observatory
  ╔══════════════════════╗                ╔══════════════════════╗                
  ║                      ║                ║                      ║                
  ║                      ║                ║                      ║                
- ║                      ╠──┐          ┌──╣                      ║         
- ║  Source              │  │────────▶|   │   linesToStations    ║ 
- ║     .fromIterator()  ╠──┘          └──╣                      ║ 
+ ║  Stations            ╠──┐          ┌──╣                      ║         
+ ║     .fromIterator()      ────────▶        linesToStations    ║ 
+ ║                      ╠──┘          └──╣                      ║ 
  ║                      ║                ║                      ║  
  ║                      ║                ║                      ║   
  ║                      ║                ║                      ║   
